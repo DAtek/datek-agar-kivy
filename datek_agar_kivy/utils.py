47838931a0df01f06ce977da74745965ccab8c80
Attila Dudas <dudasa7@gmail.com>
2022-02-06 11:30:54 +0100

    Add black code style

--- datek_agar_kivy/utils.py
+++ datek_agar_kivy/utils.py
@@ -6,7 +6,9 @@ INITIAL_SIZE = 30
 INITIAL_CORRECTION = round(-INITIAL_SIZE / 2)
 
 
-def calculate_corrected_positions(position_pixel_coordinates: np.ndarray, radius_pixels: np.ndarray) -> np.ndarray:
+def calculate_corrected_positions(
+    position_pixel_coordinates: np.ndarray, radius_pixels: np.ndarray
+) -> np.ndarray:
     positions = position_pixel_coordinates.copy()
     positions[:, 0] -= radius_pixels
     positions[:, 1] -= radius_pixels
